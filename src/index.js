76b34fbae2ce2aa66e96d91300a0a8ce6745b534
khaled22 <khaled@sherzal.com>
2022-02-04 20:20:59 +0200

    adding TimeTable class

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import TimeTable from './TimeTable';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TimeTable />
   </React.StrictMode>,
   document.getElementById('root')
 );
