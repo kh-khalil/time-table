1f4735597da7d4e4bb8e4e89eb036c66b21bd12c
khaled22 <khaled@sherzal.com>
2022-02-06 21:40:05 +0200

    adding toasters for adding, editing & deleting appointment

--- src/App.js
+++ src/App.js
@@ -3,16 +3,35 @@ import 'devextreme/dist/css/dx.light.css';
 import { useState, useCallback } from 'react';
 
 import { Scheduler, View, Editing } from 'devextreme-react/scheduler';
+import notify from 'devextreme/ui/notify';
 
 import { appointments } from './appointments';
 
 function App() {
+
+
    const [currentDate, setCurrentDate] = useState(new Date());
    const handlePropertyChange = useCallback((e) => {
       if (e.name === 'currentDate') {
          setCurrentDate(e.value);
       }
    }, [])
+
+   function showToast(event, value, type) {
+      notify(`${event} "${value}" task`, type, 800);
+   }
+
+   function showAddedToast(e) {
+      showToast('Added', e.appointmentData.text, 'success');
+   }
+
+   function showUpdatedToast(e) {
+      showToast('Updated', e.appointmentData.text, 'info');
+   }
+
+   function showDeletedToast(e) {
+      showToast('Deleted', e.appointmentData.text, 'warning');
+   }
 
    return (
       <Scheduler
@@ -28,16 +47,19 @@ function App() {
          showCurrentTimeIndicator={true}
          shadeUntilCurrentTime={true}
          allowDragging={true}
+         onAppointmentAdded={showAddedToast}
+         onAppointmentUpdated={showUpdatedToast}
+         onAppointmentDeleted={showDeletedToast}
       >
          <View
             type="day"
-            startDayHour={10}
-            endDayHour={22}
+            startDayHour={9}
+            endDayHour={19}
          />
          <View
             type="week"
-            startDayHour={10}
-            endDayHour={22}
+            startDayHour={9}
+            endDayHour={19}
          />
          <View type="month" />
          <Editing
