8c17fa099155c3be6c60aee31981154db4406a65
khaled22 <khaled@sherzal.com>
2022-02-07 22:57:16 +0200

    removing home page & updating nav btns bet 2 approaches

--- src/Container/Container.js
+++ src/Container/Container.js
@@ -6,37 +6,37 @@ import './container.css';
 
 export default function Container() {
 
-   const [view, setView] = useState('home');
+   const [view, setView] = useState('firstApproach');
    function changeView(requiredView) {
       setView(requiredView);
    }
 
    if (view === 'home') {
-      return (
-         <div className='home-container'>
-            <Button
-               text="First Approach"
-               type="normal"
-               stylingMode="outlined"
-               onClick={() => changeView('firstApproach')}
-            />
-            <Button
-               text="Second Approach"
-               type="normal"
-               stylingMode="outlined"
-               onClick={() => changeView('secondApproach')}
-            />
-         </div>
-      )
+      // return (
+      //    <div className='home-container'>
+      //       <Button
+      //          text="First Approach"
+      //          type="normal"
+      //          stylingMode="outlined"
+      //          onClick={() => changeView('firstApproach')}
+      //       />
+      //       <Button
+      //          text="Second Approach"
+      //          type="normal"
+      //          stylingMode="outlined"
+      //          onClick={() => changeView('secondApproach')}
+      //       />
+      //    </div>
+      // )
    } else if (view === 'firstApproach') {
       return (
          <div className='approach-container'>
             <Button
                className='approach-btn'
-               text="Return Home"
+               text="Second Approach"
                type="success"
                stylingMode="contained"
-               onClick={() => changeView('home')}
+               onClick={() => changeView('secondApproach')}
             />
             <a target="_blank" rel="noopener noreferrer" style={{ paddingRight: '0.5rem' }} href='https://devexpress.github.io/devextreme-reactive/react/scheduler/demos/featured/data-editing/'>DevExpress Demo</a>
             <a target="_blank" rel="noopener noreferrer" href='https://devexpress.github.io/devextreme-reactive/react/scheduler/docs/guides/date-navigation/'>Scheduler Guide & API</a>
@@ -47,10 +47,10 @@ export default function Container() {
       <div className='approach-container'>
          <Button
             className='approach-btn'
-            text="Return Home"
+            text="First Approach"
             type="success"
             stylingMode="contained"
-            onClick={() => changeView('home')}
+            onClick={() => changeView('firstApproach')}
          />
          <a target="_blank" rel="noopener noreferrer" style={{ paddingRight: '0.5rem' }} href='https://js.devexpress.com/Demos/WidgetsGallery/Demo/Scheduler/SimpleArray/React/Light/'>DevExtreme Demo</a>
          <a target="_blank" rel="noopener noreferrer" href='https://js.devexpress.com/Documentation/ApiReference/Common/Object_Structures/dxSchedulerAppointment/#allDay'>Scheduler API</a>
